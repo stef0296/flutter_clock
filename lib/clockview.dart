8f75aefad85fa84c17a3790a406807661cf2188d
stef0296 <steff.alvares@gmail.com>
2020-07-04 13:12:53 +0530

    Initial commit

---
+++ lib/clockview.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_clock/clockPainter.dart';
+
+class ClockView extends StatefulWidget {
+  @override
+  _ClockViewState createState() => _ClockViewState();
+}
+
+class _ClockViewState extends State<ClockView> {
+  @override
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 1), (_) => setState(() {}));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 300,
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
+      ),
+    );
+  }
+}
